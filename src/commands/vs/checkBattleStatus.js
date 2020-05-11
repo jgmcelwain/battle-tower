796508dda5409cc2b0944cd1a5839d8fb98eaaac
Jamie McElwain <jgmcelwain@icloud.com>
2020-05-11 17:51:21 +0100

    feat: use playerInBattle when checking battle status of players

--- src/commands/vs/checkBattleStatus.js
+++ src/commands/vs/checkBattleStatus.js
@@ -2,11 +2,7 @@ import db from '../../db/index.js';
 import logger from '../../logger.js';
 
 export default async function checkBattleStatus(players) {
-  const inBattleRequests = players.map(async (player) => {
-    const currentBattle = await db.GET_CURRENT_BATTLE(player);
-
-    return currentBattle !== null;
-  });
+  const inBattleRequests = players.map((player) => db.PLAYER_IN_BATTLE(player));
 
   logger.log('info', `Battle statuses requested for ${players.join(', ')}`);
   const result = await Promise.all(inBattleRequests);
